aebcad53efb9fa5cfba1f06f7e5b74b3833438fa
Himanshu Gaurav Singh <cinnabar233@gmail.com>
2021-03-27 19:28:27 +0530

    filename from command line

--- plotter2.py
+++ plotter2.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import pandas as pd
-df=pd.read_csv("out_2.txt")
+import sys
+df=pd.read_csv(sys.argv[1])
 ax = plt.gca()
 #df.plot(kind='line',x='frame',y='dynamic density', color = 'blue' , ax=ax)
 df.plot(kind='line',x='frame',y='queue density', color = 'red' , ax=ax)
